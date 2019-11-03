ae279582d748a67c797c3c9612126a3af39dbc2b
naftali kugelman <naftul6@gmail.com>
2019-11-03 16:45:32 +0200

    in

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean[][] mat = new Boolean[12][];
+            for (int i = 0; i < mat.Length; i++)
+            {
+                mat[i] = new Boolean[31];
+                for (int j = 0; j < mat[i].Length; j++)
+                {
+                    mat[i][j] = false;
+                }
+            }
+            //print
+            for (int i = 0; i < mat.Length; i++)
+            {
+                for (int j = 0; j < mat[i].Length; j++)
+                {
+                    Console.Write(" {0}",mat[i][j]);
+                }
+                Console.WriteLine();
+            }
+
+            DateTime date = new DateTime();
+            Console.Write("Enter your start date: ");
+            date = Convert.ToDateTime(Console.ReadLine());
+            //Console.WriteLine(date);
+
+            Console.Write("Enter how much days: ");
+            int days = Convert.ToInt32(Console.ReadLine());
+            
+            //check is aveilable
+            DateTime tempDate = new DateTime();
+            tempDate = date;
+            int countDays = 1;
+            tempDate.AddDays(1);
+            for(int i = 0; i < days; i++)
+            {
+                if (mat[tempDate.Month][tempDate.Day] = false)
+                {
+                    tempDate.AddDays(1);
+                    countDays++;
+                };
+            }
+
+
+
+        }
+    }
+}
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -8,8 +8,39 @@ namespace ConsoleApp6
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            //Lottery numbers randomly
+            Random rnd = new Random(DateTime.Now.Millisecond);
+
+            //three arrays
+            int[] A = new int[20];
+            int[] B = new int[20];
+            int[] C = new int[20];
+            Console.Write("A:");
+            for (int i = 0; i < 20; i++)
+            {
+                A[i] = rnd.Next() % 102 + 18;
+                Console.Write("  {0,-3}", A[i]);
+            }
+            Console.WriteLine();
+            Console.Write("B:");
+            for (int i = 0; i < 20; i++)
+            {
+                B[i] = rnd.Next() % 102 + 18;
+                Console.Write("  {0,-3}", B[i]);
+            }
+            Console.WriteLine();
+            Console.Write("C:");
+
+            //absolute diference value between array A and array B
+            for (int i = 0; i < 20; i++)
+            {
+                C[i] = Math.Abs(B[i]-A[i]);
+                Console.Write("  {0,-3}", C[i]);
+            }
+            Console.WriteLine();
         }
     }
 }
